02bd1d2424bf16219415c0274315beec7448314a
Siddharth Gupta <siddharthgupta3010@gmail.com>
2017-03-02 00:37:25 +0530

    Update friendfunc_class_cmplx.cpp

--- friendfunc_class_cmplx.cpp
+++ friendfunc_class_cmplx.cpp
@@ -10,6 +10,7 @@ class Complex
 	public:
 		void getData(void);
 		friend void addComplex(Complex,Complex);
+		friend void subtractComplex(Complex,Complex);
 };
 
 void Complex::getData(void)
@@ -26,12 +27,18 @@ void addComplex(Complex a,Complex b)
 	cout<<"\nSum is "<<sum.iReal<<"+i"<<sum.iImag<<endl;
 }
 
+void subtractComplex(Complex a,Complex b)
+{
+	Complex subb;
+	subb.iReal=a.iReal-b.iReal;
+	subb.iImag=a.iImag-b.iImag;
 int main()
 {
 	Complex c1,c2;
 	c1.getData();
 	c2.getData();
 	addComplex(c1,c2);
+	subtractComplex(c1,c2);
 	cin.get();
 	return 0;
 }
