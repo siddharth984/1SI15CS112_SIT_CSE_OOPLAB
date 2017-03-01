e627a67df681f2861842658c7baba3f7089fc412
Siddharth Gupta <siddharthgupta3010@gmail.com>
2017-03-02 00:17:18 +0530

    Create friendfunc_class_cmplx.cpp

---
+++ friendfunc_class_cmplx.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+class Complex
+{
+	private:
+		int iReal;
+		int iImag;
+	public:
+		void getData(void);
+		friend void addComplex(Complex,Complex);
+};
+
+void Complex::getData(void)
+{
+	cout<<"\nEnter real and imaginary parts of complex number"<<endl;
+	cin>>iReal>>iImag;
+}
+
+void addComplex(Complex a,Complex b)
+{	Complex sum;
+	sum.iReal=a.iReal+b.iReal;
+	sum.iImag=a.iImag+b.iImag;
+	
+	cout<<"\nSum is "<<sum.iReal<<"+i"<<sum.iImag<<endl;
+}
+
+int main()
+{
+	Complex c1,c2;
+	
+	c1.getData();
+	c2.getData();
+	addComplex(c1,c2);
+	
+	cin.get();
+	return 0;
+}
